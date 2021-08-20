64b5364b3072d2fd18dfb3064637a532e2b03053
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2021-08-20 13:32:05 +0100

    Enabled IptcData assignment

---
+++ examples/iptceasy.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# python-exiv2 - Python interface to libexiv2
+# http://github.com/jim-easterbrook/python-exiv2
+# Copyright (C) 2021  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# The quickest way to access, set or modify IPTC metadata.
+
+import sys
+
+import exiv2
+
+def main():
+    try:
+        exiv2.XmpParser.initialize()
+        if len(sys.argv) != 2:
+            print('Usage: {} file'.format(sys.argv[0]))
+            return 1;
+        file = sys.argv[1]
+
+        iptcData = exiv2.IptcData()
+
+        iptcData["Iptc.Application2.Headline"] = "The headline I am"
+        iptcData["Iptc.Application2.Keywords"] = "Yet another keyword"
+        iptcData["Iptc.Application2.DateCreated"] = "2004-8-3"
+        iptcData["Iptc.Application2.Urgency"] = exiv2.UShortValue(1)
+        iptcData["Iptc.Envelope.ModelVersion"] = exiv2.UShortValue(42)
+        iptcData["Iptc.Envelope.TimeSent"] = "14:41:0-05:00"
+        iptcData["Iptc.Application2.RasterizedCaption"] = "230 42 34 2 90 84 23 146"
+        iptcData["Iptc.0x0009.0x0001"] = "Who am I?"
+
+        value = exiv2.StringValue()
+        value.read("very!")
+        iptcData["Iptc.Application2.Urgency"] = value
+
+        print("Time sent: {}".format(iptcData["Iptc.Envelope.TimeSent"]))
+
+        # Open image file
+        image = exiv2.ImageFactory.open(file)
+
+        # Set IPTC data and write it to the file
+        image.setIptcData(iptcData)
+        image.writeMetadata()
+
+        return 0
+    except exiv2.AnyError as e:
+        print('Caught Exiv2 exception "{}"'.format(str(e)))
+        return -1;
+    finally:
+        exiv2.XmpParser.terminate()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
--- src/iptc.i
+++ src/iptc.i
@@ -21,13 +21,49 @@
 
 %include "preamble.i"
 
+%include "stdint.i"
+%include "std_string.i"
+
 %import "datasets.i"
 %import "metadatum.i"
 
-%include "std_string.i"
-
 GETITEM(Exiv2::IptcData, Exiv2::Iptcdatum)
 ITERATOR(Exiv2::IptcData, Exiv2::Iptcdatum, IptcDataIterator)
+STR(Exiv2::Iptcdatum, toString)
+
+%feature("python:slot", "mp_ass_subscript",
+         functype="objobjargproc") Exiv2::IptcData::__setitem__;
+
+%extend Exiv2::IptcData {
+    PyObject* __setitem__(const std::string& key, const Exiv2::Iptcdatum &rhs) {
+        using namespace Exiv2;
+        IptcKey k = IptcKey(key);
+        TypeId type_id = IptcDataSets::dataSetType(k.tag(), k.record());
+        if (type_id != rhs.typeId()) {
+            EXV_WARNING << key << ": type change from '" <<
+                TypeInfo::typeName(type_id) << "' to '" <<
+                rhs.typeName() << "'.\n";
+        }
+        (*($self))[key] = rhs;
+        return SWIG_Py_Void();
+    }
+    PyObject* __setitem__(const std::string& key, const Exiv2::Value &value) {
+        using namespace Exiv2;
+        IptcKey k = IptcKey(key);
+        TypeId type_id = IptcDataSets::dataSetType(k.tag(), k.record());
+        if (type_id != value.typeId()) {
+            EXV_WARNING << key << ": type change from '" <<
+                TypeInfo::typeName(type_id) << "' to '" <<
+                TypeInfo::typeName(value.typeId()) << "'.\n";
+        }
+        (*($self))[key] = value;
+        return SWIG_Py_Void();
+    }
+    PyObject* __setitem__(const std::string& key, const std::string &value) {
+        (*($self))[key] = value;
+        return SWIG_Py_Void();
+    }
+}
 
 %ignore Exiv2::IptcData::begin() const;
 %ignore Exiv2::IptcData::end() const;
