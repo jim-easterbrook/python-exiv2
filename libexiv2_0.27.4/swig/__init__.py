259a60adf8e358ecf02a9f33f02e078921602ded
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2021-09-03 08:37:00 +0100

    Add SWIG generated files to git

---
+++ libexiv2_0.27.4/swig/__init__.py
@@ -0,0 +1,35 @@
+
+import logging
+import sys
+
+if sys.platform == 'linux':
+    import os
+    _dir = os.path.dirname(__file__)
+    for _file in os.listdir(_dir):
+        if _file.startswith('libexiv2.so'):
+            # import libexiv2 shared library (avoids setting LD_LIBRARY_PATH)
+            from ctypes import cdll
+            cdll.LoadLibrary(os.path.join(_dir, _file))
+
+_logger = logging.getLogger(__name__)
+
+class AnyError(Exception):
+    """Python exception raised by exiv2 library errors"""
+    pass
+
+__version__ = "0.0.0"
+
+from exiv2.datasets import *
+from exiv2.error import *
+from exiv2.exif import *
+from exiv2.image import *
+from exiv2.iptc import *
+from exiv2.metadatum import *
+from exiv2.properties import *
+from exiv2.tags import *
+from exiv2.types import *
+from exiv2.value import *
+from exiv2.version import *
+from exiv2.xmp import *
+
+__all__ = [x for x in dir() if x[0] != '_']
