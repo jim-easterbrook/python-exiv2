434fff22d9ae7d8c97ddb703c78352eaefabae31
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2021-09-06 13:29:40 +0100

    Increment version number

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-python-exiv2 v\ 0.2.2
+python-exiv2 v\ 0.2.3
 =====================
 
 python-exiv2 is a low level interface (or binding) to the exiv2_ C++ library.
--- libexiv2_0.26/swig/__init__.py
+++ libexiv2_0.26/swig/__init__.py
@@ -16,7 +16,7 @@ class AnyError(Exception):
     """Python exception raised by exiv2 library errors"""
     pass
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from exiv2.datasets import *
 from exiv2.error import *
