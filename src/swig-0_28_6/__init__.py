c2281f1c705dc8acc3aca40041d342dd4386d001
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2025-08-30 10:58:32 +0100

    Update python-exiv2 version

--- src/swig-0_28_6/__init__.py
+++ src/swig-0_28_6/__init__.py
@@ -20,9 +20,9 @@ class Exiv2Error(Exception):
         self.message = message
 
 #: python-exiv2 version as a string
-__version__ = "0.17.3"
+__version__ = "0.17.4"
 #: python-exiv2 version as a tuple of ints
-__version_tuple__ = tuple((0, 17, 3))
+__version_tuple__ = tuple((0, 17, 4))
 
 __all__ = ["Exiv2Error"]
 from exiv2.basicio import *
