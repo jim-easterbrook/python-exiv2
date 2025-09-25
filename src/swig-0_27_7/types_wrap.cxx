038e0c388d4d971f094eb0a30ed557f51d3cc9e6
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2025-09-25 08:27:00 +0100

    Minor improvement of "private" data store

--- src/swig-0_27_7/types_wrap.cxx
+++ src/swig-0_27_7/types_wrap.cxx
@@ -4763,11 +4763,12 @@ SWIG_AsVal_long (PyObject *obj, long* val)
 
 
 static PyObject* _get_store(PyObject* py_self, bool create) {
-    // Return a new reference
+    // Return a borrowed reference
+    PyObject* dict = NULL;
     if (!PyObject_HasAttrString(py_self, "_private_data_")) {
         if (!create)
             return NULL;
-        PyObject* dict = PyDict_New();
+        dict = PyDict_New();
         if (!dict)
             return NULL;
         int error = PyObject_SetAttrString(py_self, "_private_data_", dict);
@@ -4775,35 +4776,31 @@ static PyObject* _get_store(PyObject* py_self, bool create) {
         if (error)
             return NULL;
     }
-    return PyObject_GetAttrString(py_self, "_private_data_");
+    dict = PyObject_GetAttrString(py_self, "_private_data_");
+    Py_DECREF(dict);
+    return dict;
 };
 static int private_store_set(PyObject* py_self, const char* name,
                              PyObject* val) {
     PyObject* dict = _get_store(py_self, true);
     if (!dict)
         return -1;
-    int result = PyDict_SetItemString(dict, name, val);
-    Py_DECREF(dict);
-    return result;
+    return PyDict_SetItemString(dict, name, val);
 };
 static PyObject* private_store_get(PyObject* py_self, const char* name) {
     // Return a borrowed reference
     PyObject* dict = _get_store(py_self, false);
     if (!dict)
         return NULL;
-    PyObject* result = PyDict_GetItemString(dict, name);
-    Py_DECREF(dict);
-    return result;
+    return PyDict_GetItemString(dict, name);
 };
 static int private_store_del(PyObject* py_self, const char* name) {
     PyObject* dict = _get_store(py_self, false);
     if (!dict)
         return 0;
-    int result = 0;
     if (PyDict_GetItemString(dict, name))
-        result = PyDict_DelItemString(dict, name);
-    Py_DECREF(dict);
-    return result;
+        return PyDict_DelItemString(dict, name);
+    return 0;
 };
 
 
