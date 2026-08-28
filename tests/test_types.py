7f9947f29d39e1439a6cf31b935628e1779ded22
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2026-08-28 11:58:42 +0100

    Put language back to English after localisation test

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 ##  python-exiv2 - Python interface to libexiv2
 ##  http://github.com/jim-easterbrook/python-exiv2
-##  Copyright (C) 2023-25  Jim Easterbrook  jim@jim-easterbrook.me.uk
+##  Copyright (C) 2023-26  Jim Easterbrook  jim@jim-easterbrook.me.uk
 ##
 ##  This program is free software: you can redistribute it and/or
 ##  modify it under the terms of the GNU General Public License as
@@ -163,6 +163,7 @@ class TestTypesModule(unittest.TestCase):
                     key = exiv2.ExifKey(999, 'Invalid')
                 self.assertEqual(cm.exception.message.replace('"', "'"),
                                  "Ungültige IFD-ID 0")
+            set_language()
 
 
 if __name__ == '__main__':
